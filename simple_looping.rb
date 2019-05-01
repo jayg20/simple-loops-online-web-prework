f92dd5468183abcbdc1e81c52bf99213fd0bfdc9
James Gomez <james.gomez320@yahoo.com>
2019-05-01 14:59:50 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -31,7 +31,7 @@ end
 
 number_of_times = 0 
 def for_iterator(number_of_times)
-  for number_of_times(1..7)
+  for number in number_of_times(1..7)
   puts phrase
   phrase = "Welcome to Flatiron School's Web Development Course!"
 end
